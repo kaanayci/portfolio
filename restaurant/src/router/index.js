841ff2f3feb9f804377835164aeff5b472a32a9b
kaanayci <kaanayci@gmail.com>
2026-02-04 00:04:14 +0100

    feat: add admin route and navigation guard for admin access control

--- restaurant/src/router/index.js
+++ restaurant/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import { useUserStore } from '@/stores/user'
 import HomeView from '../views/HomeView.vue'
 import MenuView from '../views/MenuView.vue'
 import CartView from '../views/CartView.vue'
@@ -30,8 +31,24 @@ const router = createRouter({
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      component: () => import('../views/AdminView.vue'),
+      meta: { requiresAdmin: true }
     }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  
+  if (to.meta.requiresAdmin && !userStore.isAdmin) {
+    next('/login')
+  } else {
+    next()
+  }
 })
 
 export default router
