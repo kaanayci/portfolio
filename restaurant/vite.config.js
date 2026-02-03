afa3567bf8394a10e93566fb4bc4f4804d716851
kaanayci <kaanayci@gmail.com>
2026-02-03 16:15:15 +0100

    feat: add Vite configuration for Vue and PWA support

---
+++ restaurant/vite.config.js
@@ -0,0 +1,39 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Coin Régal',
+        short_name: 'Coin Régal',
+        description: 'Commandez vos plats préférés chez Coin Régal',
+        theme_color: '#0A1E42',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
