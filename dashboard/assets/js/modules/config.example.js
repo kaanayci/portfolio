7d085e1f0209eb5c89af6d41e6dc6250f0fd36db
kaanayci <kaanayci@gmail.com>
2026-02-04 00:47:00 +0100

    feat: add example configuration file for OpenWeatherMap API

---
+++ dashboard/assets/js/modules/config.example.js
@@ -0,0 +1,6 @@
+// IMPORTANT : Ceci est un exemple de configuration.
+// Pour que l'application fonctionne, créez un fichier "config.js" dans ce même dossier
+// et ajoutez votre clé API OpenWeatherMap.
+
+// const WEATHER_API_KEY = "VOTRE_CLE_API_ICI";
+// export { WEATHER_API_KEY };
