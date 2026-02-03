97c77304149bffeebddb18556c6196acab13a130
kaanayci <kaanayci@gmail.com>
2026-02-03 16:53:11 +0100

    feat: add CartView route for cart functionality

--- restaurant/src/router/index.js
+++ restaurant/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import MenuView from '../views/MenuView.vue'
+import CartView from '../views/CartView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(),
@@ -14,6 +15,16 @@ const router = createRouter({
       path: '/menu',
       name: 'menu',
       component: MenuView
+    },
+    {
+      path: '/panier',
+      name: 'cart',
+      component: CartView
+    },
+    {
+      path: '/checkout',
+      name: 'checkout',
+      component: () => import('../views/CheckoutView.vue') // Lazy load
     }
   ]
 })
