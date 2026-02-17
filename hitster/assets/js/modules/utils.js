12f0e87f660f416f7dd0e1bd860ec194d873e56f
Kaan Kalayci <kaan.kalayci@rpn.ch>
2026-02-17 20:43:42 +0100

    refactor: remove unnecessary comments from shuffle function

--- hitster/assets/js/modules/utils.js
+++ hitster/assets/js/modules/utils.js
@@ -1,8 +1,4 @@
-/**
- * Algorithme de mélange de Fisher-Yates
- * @param {Array} array - Le tableau à mélanger
- * @returns {Array} Le tableau mélangé
- */
+// Fisher-Yates : mélange un tableau en place en O(n)
 export function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
